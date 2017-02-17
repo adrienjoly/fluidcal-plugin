dd01ece3cac9145275ff52bde4cd46e30bea407b
Adrien Joly <adrien@whyd.com>
2017-02-17 18:44:46 +0100

    refacto: dummy_tasks

--- server-methods.js
+++ server-methods.js
@@ -1,15 +1,17 @@
+var DUMMY_TASKS = [
+  {
+    name: 'Task 1',
+    duration: { seconds: 30 * 60, nanos: 0 },
+  },
+  {
+    name: 'Task 2',
+    duration: { seconds: 45 * 60, nanos: 0 },
+  },
+];
+
 exports.getTaskSuggestions = function getTaskSuggestions(call, callback) {
   console.log('getTaskSuggestions', call.request);
   callback(null, {
-    tasks: [
-      {
-        name: 'Task 1',
-        duration: { seconds: 30 * 60, nanos: 0 },
-      },
-      {
-        name: 'Task 2',
-        duration: { seconds: 45 * 60, nanos: 0 },
-      },
-    ],
+    tasks: DUMMY_TASKS,
   });
 };
