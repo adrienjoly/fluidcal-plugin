791e68366157267153c82b907eaaa51a991871c9
Adrien Joly <adrien@whyd.com>
2017-02-17 18:52:24 +0100

    server method getTaskSuggestions() returns next steps from trello

--- server-methods.js
+++ server-methods.js
@@ -1,17 +1,37 @@
+var trelloChecklists = require('./lib/trello-checklists')
+
+const TRELLO_BOARD_ID = '57a481755ab2c09495ba5c3b' // TODO
+
+const DEFAULT_DURATION = { seconds: 30 * 60, nanos: 0 }
+
+/*
 var DUMMY_TASKS = [
   {
     name: 'Task 1',
-    duration: { seconds: 30 * 60, nanos: 0 },
+    duration: DEFAULT_DURATION,
   },
   {
     name: 'Task 2',
-    duration: { seconds: 45 * 60, nanos: 0 },
+    duration: DEFAULT_DURATION,
   },
-];
+]
+*/
+
+const translateToTask = (nextStep) => ({
+  duration: DEFAULT_DURATION,
+  name: [
+    nextStep.card.name,
+    nextStep.checklist.name,
+    nextStep.nextStep
+  ].join(' -- '), // TODO: better formating, or separate properties
+})
 
 exports.getTaskSuggestions = function getTaskSuggestions(call, callback) {
-  console.log('getTaskSuggestions', call.request);
-  callback(null, {
-    tasks: DUMMY_TASKS,
-  });
-};
+  console.log('getTaskSuggestions', call.request)
+  trelloChecklists
+    .getNextSteps(TRELLO_BOARD_ID)
+    .then((nextSteps) => nextSteps.map(translateToTask))
+    .then((tasks) =>
+      callback(null, { tasks: tasks })
+    )
+}
