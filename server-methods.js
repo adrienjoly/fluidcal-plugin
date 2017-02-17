4f0364d66a46c1eeadd4074c23a41cc0c5cdd2b7
Adrien Joly <adrien@whyd.com>
2017-02-17 17:04:05 +0100

    added sample tasks to dummy grpc method implementation

--- server-methods.js
+++ server-methods.js
@@ -2,6 +2,14 @@ exports.getTaskSuggestions = function getTaskSuggestions(call, callback) {
   console.log('getTaskSuggestions', call.request);
   callback(null, {
     tasks: [
+      {
+        name: 'Task 1',
+        duration: { seconds: 30 * 60, nanos: 0 },
+      },
+      {
+        name: 'Task 2',
+        duration: { seconds: 45 * 60, nanos: 0 },
+      },
     ],
   });
 };
