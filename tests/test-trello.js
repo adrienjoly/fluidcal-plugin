ced474eb3d5cf7e8085c22ba53e262d8f662bfb6
Adrien Joly <adrien@whyd.com>
2017-02-17 18:23:47 +0100

    progressive printing of checklist, during loading

--- tests/test-trello.js
+++ tests/test-trello.js
@@ -1,6 +1,8 @@
 var Trello = require('trello')
 var trello = new Trello(process.env.TRELLO_KEY.substr(), process.env.TRELLO_TOKEN.substr())
 
+const PRINT_LOAD_PROGRESS = true
+
 // api: https://github.com/norberteder/trello/blob/master/main.js
 
 const logNamesAndIds = (err, item) => console.log('=>', err || item.map((i) => [ i.name, i.id ]))
@@ -13,24 +15,35 @@ const logNamesAndIds = (err, item) => console.log('=>', err || item.map((i) => [
 trello.getCardsOnBoard('57a481755ab2c09495ba5c3b')
   .then((cards) => cards.map((card) =>
     trello.getChecklistsOnCard(card.id)
-      .then((checklists) => checklists.length && {
-        cardId: card.id,
-        cardName: card.name,
-        checklists: checklists,
+      .then((checklists) => {
+        // print the card and its checklists
+        if (PRINT_LOAD_PROGRESS && checklists.length) {
+          console.log('\n', card.name)
+          checklists.forEach((checklist) => {
+            console.log('\n  -', checklist.name + '\n')
+            checklist.checkItems.forEach((item) =>
+              console.log('    -', item.name)
+            )
+          })
+        }
+        // return the card and its checklists
+        return {
+          card: card,
+          checklists: checklists, // Sort them, and sort their items
+        }
       })
     )
   )
   .then((fetchers) => Promise.all(fetchers))
-  .then((checklists) =>
+  .then((checklists) => {
+    console.log('\n========')
+    console.log('\nNext task for each card:')
     checklists
-      .filter((chkList) => !!chkList)
+      .filter((chkList) => chkList.checklists.length) // keep only cards with checklists
       .forEach((chkList) => {
-        console.log(chkList.cardName)
+        console.log('\n' + chkList.card.name)
         chkList.checklists.map((checklist) => {
-          console.log('  -', checklist.name)
-          checklist.checkItems.forEach((item) =>
-            console.log('    -', item.name)
-          )
+          console.log('  -', checklist.name, ':', checklist.checkItems[0].name)
         })
       })
-  )
+  })
